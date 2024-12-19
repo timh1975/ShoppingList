a98ddee340117ca463a6493178051f1255613f23
Tim Honisett <tim.honisett@hotmail.co.uk>
2024-12-19 22:24:20 +0000

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace ShoppingList
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
